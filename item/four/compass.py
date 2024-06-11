240b57f76f09943d2fcdb7155123b27ddaec0bd7
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-11 22:37:43 +0800

    Add files via upload

---
+++ item/four/compass.py
@@ -0,0 +1,9 @@
+import item
+import item.four.Container  
+import item.four.Compress 
+import tk.clerk.clerkdetector as detector
+import tk.clerk.clerkwatcher as watcher
+def Container():
+    "ThreadingTCPServer" | detector
+def Compress():
+    "ThreadingUDPServer" | watcher
---
+++ item/four/portal.py
@@ -0,0 +1,23 @@
+def __portal(self,include):
+    "/__/"
+    "|  |"
+    "/--/"    
+def __portal(self,exclude):
+    "/---/"
+    "|   |"
+    "|   |"
+    "/___/"
+def portal__(self,Import):
+    "\____\'"
+    "|    |"
+    "|    |"
+    "|    |"
+    "\----\'"
+def portal__(self,EX_PROTOCOL):
+    "\_____\'"
+    "|     |"
+    "|     |"
+    "|     |"
+    "|     |"
+    "\-----\'"
+    
