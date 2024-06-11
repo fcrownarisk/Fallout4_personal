17261a160b9017ab5a0f297ce2b6149a3f5abedf
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-11 18:01:55 +0800

    Add files via upload

---
+++ InventorySlot/weapon.py
@@ -0,0 +1,15 @@
+import random
+import Inventory_ii
+import Slot_iii
+import InventorySlot
+import InventorySlot_i
+weapon_types = ['']
+prefix = ['traditional','advanced','well-known','high-energy','masterpiece']
+suffix = ['shooter','sniper','accurate','boom']
+rarity = ['common','rare','epic','lengedary','unique','career','godly']
+def GeneratorType(self,weapon):
+    return prefix + suffix
+def getgeneratorstate(self,InventorySlot,InventorySlot_i):
+    return ['prefix:rarity','rarity:suffix']
+def weaponprototype(self,inventory_ii,Slot_iii): 
+    pass
