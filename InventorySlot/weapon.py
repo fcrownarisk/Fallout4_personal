43d3fc6d7c9c58182e705c5ae3b595bd4902a880
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-11 22:39:47 +0800

    Update weapon.py

--- InventorySlot/weapon.py
+++ InventorySlot/weapon.py
@@ -1,15 +1,21 @@
 import random
-import Inventory_ii
-import Slot_iii
+import InventorySlot.InventorySlot
+import InventorySlot.Slot_iii
 import InventorySlot
-import InventorySlot_i
-weapon_types = ['']
-prefix = ['traditional','advanced','well-known','high-energy','masterpiece']
-suffix = ['shooter','sniper','accurate','boom']
+import InventorySlot.InventorySlot_i
+weapon_types = ['melee','ranged','magic','light','highlight']
+prefix = ['most-used','advanced','low-energy','well-known','high-energy','master-piece']
+suffix = ['shooter','sniper','accurate','','','boom','melee']
 rarity = ['common','rare','epic','lengedary','unique','career','godly']
 def GeneratorType(self,weapon):
     return prefix + suffix
-def getgeneratorstate(self,InventorySlot,InventorySlot_i):
-    return ['prefix:rarity','rarity:suffix']
-def weaponprototype(self,inventory_ii,Slot_iii): 
-    pass
+def weapontype(self,weapon):
+    return weapon_types
+def rarity(self,weapon):
+    return rarity
+def generator(self,InventorySlot,InventorySlot_i):
+    return InventorySlot
+    InventorySlot_i
+def weaponprototype(self,Inventory_ii,Slot_iii): 
+    return Inventory_ii
+    Slot_iii
