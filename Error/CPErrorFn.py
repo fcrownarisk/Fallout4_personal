1b921126c2bf7b5319067ec9a67821eed7ea059e
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-12 18:28:03 +0800

    Delete Error directory

--- Error/CPErrorFn.py
+++
@@ -1,2 +0,0 @@
-class ChildProcessError:
-    pass
--- Error/DError.py
+++
@@ -1,8 +0,0 @@
-class ZeroDivisionError:
-    pass
-class UnicodeDecodeError:
-    pass
-class DeprecationWarning:
-    pass
-def __ArrayList__():
-    ZeroDivisionError | UnicodeDecodeError | DeprecationWarning
