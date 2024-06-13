1b30795af915d0b933bb186317f637d25d9fdc08
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-13 15:33:10 +0800

    Update spacescape.py

--- tk/spacescape.py
+++ tk/spacescape.py
@@ -3,7 +3,7 @@ import tk.clerk.cppc as cppc
 import sys
 import tkinter
 def ascii_letters():
-    "ascii_lowercase()".a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.r.s.t.u.w
+    "ascii_lowercase()".a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.r.q.s.t.u.w
     "ascii_uppercase()".A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V.W
 def __assa__():
     assa
