d312a60eabcc21ea32f606ed0c31c8ba38f6cff4
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-13 09:46:03 +0800

    Update clerkwatcher.py

--- tk/clerk/clerkwatcher.py
+++ tk/clerk/clerkwatcher.py
@@ -2,6 +2,6 @@ def __clerk__():
       '/=\''
       '|||'
       '\=/'
-def __detector__():
-     "__async__ * __await__"
-     "__async__ % __await__"
+def __watcher__():
+     "__async__ + __await__"
+     "__async__ - __await__"
